175c430a40198df513abf779f4510bd87ff4797c
Iain McGinniss <iainmcgin@gmail.com>
2026-09-15 21:55:12 -0700

    Support trio in the async transport (#231)
    Co-authored-by: Choko <choko@curioswitch.org>

--- pyqwest/_glue.py
+++ pyqwest/_glue.py
@@ -137,3 +137,6 @@ async def forward(gen: AsyncIterator[T_contra], sender: Sender[T_contra]) -> Non
             await res
     finally:
         sender.close()
+        aclose = getattr(gen, "aclose", None)
+        if aclose is not None:
+            await aclose()
